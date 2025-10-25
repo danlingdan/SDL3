26df256d748016567da802d212868790f5ee203f
HuaJH <2844344532@qq.com>
2025-10-25 21:05:41 +0800

    Add SceneTitle class and integrate title screen
    Introduced a new `SceneTitle` class to implement a dynamic title screen displaying the game title and high score. Updated `game.cpp` to initialize the game with `SceneTitle` as the starting scene.
    
    Added `scene_title.cpp` and `scene_title.h` to the project, including them in `SDL3.vcxproj` and `SDL3.vcxproj.filters` for proper build and organization.
    
    Implemented `SceneTitle` methods for initialization, event handling, updates, rendering, and cleanup. Added dynamic boundary color effects using `renderBackground` and `updateColor` methods.
    
    These changes enhance the user interface with a visually appealing title screen.

---
+++ src/scene_title.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "core/scene.h"
+
+class SceneTitle : public Scene
+{
+protected:
+    SDL_FColor boundary_color_ = { 0.5, 0.5, 0.5, 1 };
+    float color_timer_ = 0;
+
+public:
+    void init() override;
+    void handleEvents(SDL_Event& event) override;
+    void update(float dt) override;
+    void render() override;
+    void clean() override;
+
+private:
+    void renderBackground();
+    void updateColor();
+};
